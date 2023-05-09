b5573c5d96b70ed675c16925e0aab055aaae62fc
Ephriam Henderson <orik162@gmail.com>
2023-05-08 20:17:19 -0500

    Add create tags.

--- GalleriOpusServer/src/Database/tags.ts
+++ GalleriOpusServer/src/Database/tags.ts
@@ -5,3 +5,16 @@ const TagRepo = db.getRepository(Tag);
 export const getTags = () => {
 	return TagRepo.find();
 };
+
+interface CreateTagsParams {
+	tags: string[]
+}
+
+export const createTags = ({ tags }: CreateTagsParams) => {
+	const newTags = tags.map(t => {
+		const tag = new Tag();
+		tag.value = t;
+		return tag
+	})
+	return TagRepo.save(newTags)
+}
--- GalleriOpusServer/src/controllers/tags/index.ts
+++ GalleriOpusServer/src/controllers/tags/index.ts
@@ -1,11 +1,22 @@
-import { getTags } from "../../Database/tags";
+import { t } from "elysia";
+import { createTags, getTags } from "../../Database/tags";
 import { Controller } from "../../types/Controller";
 
 export const tagController: Controller = (app) => {
 	app.group("/tags", (app) =>
-		app.get("/", () => {
-			return getTags();
-		})
+		app
+			.get("/", () => {
+				return getTags();
+			})
+			.post("/",  ({ body }) => {
+				return createTags({ tags: body.tags })
+			}, {
+				schema: {
+					body: t.Object({
+						tags: t.Array(t.String())
+					})
+				}
+			})
 	);
 
 	return app;
