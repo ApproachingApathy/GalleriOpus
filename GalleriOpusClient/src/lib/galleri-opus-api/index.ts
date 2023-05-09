7a66bc5302cb68c5b1e552f36e79f75a2b1da8eb
Ephriam Henderson <orik162@gmail.com>
2023-05-09 16:33:02 -0500

    feat(client) Add main list of images.

--- GalleriOpusClient/src/lib/galleri-opus-api/index.ts
+++ GalleriOpusClient/src/lib/galleri-opus-api/index.ts
@@ -1,4 +1,4 @@
-import { edenTreaty } from "@elysiajs/eden"
+import { edenFetch, edenTreaty } from "@elysiajs/eden"
 import { type App } from "../../../../GalleriOpusServer/src/index"
 
 export const api = edenTreaty<App>("http://127.0.0.1:3000")
--- GalleriOpusClient/src/pages/index.svelte
+++ GalleriOpusClient/src/pages/index.svelte
@@ -1,11 +1,32 @@
 <script lang="ts">
     import { api } from "$lib/galleri-opus-api/index"
+    import { url } from "@roxi/routify";
 
-    let data = api.index.get()
+    let data = api.assets.get().then(v => v.data).then(v => v);
+    $: console.log(data)
 </script>
 
-<h1 class="text-xl">Hello World</h1>
+{#await data then assets}
+    {console.log(assets)}
+    <div class="grid grid-flow-col gap-2 w-full grid-cols-5 md:grid-cols-6 lg:grid-cols-7">
+        {#each assets as asset (asset.id)}
+        <div class="">
+            <a class="contents" href={`/asset/${asset.id}`}>
+                <div>
+                    <img class="w-full object-contain flex-shrink h-80" src={`http://127.0.0.1:3000/assets/${asset.id}/image`} />
+                </div>    
+                <div class="flex">
+                    <div class="w-40">
+                        <p>{asset.id}</p>
+                    </div>
+                    <div class="overflow-hidden">
+                        <p class="truncate">
 
-{#await data then value} 
-    <p>{value.data}</p>
+                        </p>
+                    </div>
+                </div>
+            </a>
+            </div>
+        {/each}
+    </div>
 {/await}
