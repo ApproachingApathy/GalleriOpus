5cf9e17a66fca7b13fb3afc15ee135dc062d813d
Ephriam Henderson <orik162@gmail.com>
2023-05-10 23:54:27 -0500

    feat(client) Add keyboard event to assets.

--- GalleriOpusClient/src/pages/index.svelte
+++ GalleriOpusClient/src/pages/index.svelte
@@ -19,15 +19,23 @@
 {#if $queryResult.isSuccess}
     <div class="grid auto-rows-auto gap-2 w-full grid-cols-5 md:grid-cols-6 lg:grid-cols-7">
         {#each $queryResult.data as asset (asset.id)}
-            <div class="flex flex-col items-start p-1 {isSelected(asset) ? 'bg-teal-700' : ''}" role="button" tabindex="0" on:keyup={()=>{}} on:click={() => {
-                // console.log(asset)
+            <div class="flex flex-col p-1 {isSelected(asset) ? 'bg-teal-700' : ''}" role="button" tabindex="0" on:keyup={(e) => {
+                switch (e.key) {
+                    case " ":
+                    case "Enter":
+                        selection.set(asset)
+                    default:
+                        break;
+                }
+            }} 
+            on:click={() => {
                 selection.set(asset)
             }}>
-                <div class="h-5/6">
-                    <img class="h-full object-contain flex-shrink" src="http://127.0.0.1:3000/assets/{asset.id}/image" loading="lazy" alt=""/>
+                <div class="h-5/6 flex justify-center items-center">
+                    <img class="h-full object-contain" src="http://127.0.0.1:3000/assets/{asset.id}/image" loading="lazy" alt=""/>
                 </div>    
                 <div class="flex p-2">
-                    <div class="w-40">
+                    <div class="w-full">
                         <p>{asset.id}</p>
                     </div>
                     <div class="overflow-hidden">
